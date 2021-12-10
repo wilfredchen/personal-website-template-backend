2ba86662bf43ce5d96e89a7248ea791bea3796b1
wilfredchen <wilfredchen@gmail.com>
2021-12-11 01:12:06 +0630

    updated settings.example

--- personalsite_admin/settings.example.py
+++ personalsite_admin/settings.example.py
@@ -59,7 +59,8 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',    
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',  
+    'corsheaders.middleware.CorsMiddleware',  
 ]
 
 ROOT_URLCONF = 'personalsite_admin.urls'
